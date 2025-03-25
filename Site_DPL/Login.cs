9dceb4cbf936e636fcaa930b012ce7e038c25d04
JuanCB1 <juancarlosashcb@gmail.com>
2025-03-24 21:12:20 -0300

    Tela_DPL

--- Site_DPL/Login.cs
+++ Site_DPL/Login.cs
@@ -17,10 +17,6 @@ namespace Site_DPL
         public Login()
         {
             InitializeComponent();
-            btnEntrar.Enabled = false;
-            txtUsuario.TextChanged += ValidarCampos;
-            txtSenha.TextChanged += ValidarCampos;
-            txtSenha2.TextChanged += ValidarCampos;
         }
 
         [DllImport("user32.dll")]
@@ -47,65 +43,25 @@ namespace Site_DPL
 
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-            string user = txtUsuario.Text;
-            string email = txtEmail.Text;
-            string pass = txtSenha.Text;
-            string pass2 = txtSenha2.Text;
-
-            if (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(pass))
-            {
-                MessageBox.Show("Preencha todos os campos antes de continuar.");
-                return;
-            }
-
-            if (pass != pass2)
-            {
-                MessageBox.Show("As senhas não coincidem.");
-                return;
-            }
-
-            if (!txtEmail.Text.Contains("@") || !txtEmail.Text.Contains(".com"))
-            {
-                MessageBox.Show("E-mail inválido!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            SaveUser(user, email, pass);
-            MessageBox.Show("Cadastro realizado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-            this.Close();
 
             Form1 form1 = new Form1();
             form1.Show();
             this.Hide();
         }
-        private void SaveUser(string user, string email, string pass)
-        {
-            try
-            {
-                using (StreamWriter sw = new StreamWriter("users.txt", true))
-                {
-                    sw.WriteLine($"{user},{email},{pass}");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Erro ao salvar usuário: " + ex.Message);
-            }
-        }
+
         private void ValidarCampos(object sender, EventArgs e)
         {
-            btnEntrar.Enabled = !string.IsNullOrWhiteSpace(txtUsuario.Text) && !string.IsNullOrWhiteSpace(txtSenha.Text) && !string.IsNullOrWhiteSpace(txtSenha2.Text) && !string.IsNullOrWhiteSpace(txtEmail.Text);
+
         }
 
         private void chbSenha_CheckedChanged(object sender, EventArgs e)
         {
-            txtSenha2.UseSystemPasswordChar = !chbSenha2.Checked;
+
         }
 
         private void chbSenha2_CheckedChanged(object sender, EventArgs e)
         {
-            txtSenha2.UseSystemPasswordChar = !chbSenha2.Checked;
+
         }
     }
 }
